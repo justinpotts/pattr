43f0059a45e8d619e48d60779f8d51c0330aab87
Justin Potts <justinpotts98@gmail.com>
2015-11-11 10:31:27 -0600

    Add Heroku support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python pattr.py
--- pattr.py
+++ pattr.py
@@ -1,60 +1,35 @@
-#!/usr/bin/env python
-
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on available packages.
-async_mode = None
-
-if async_mode is None:
-    try:
-        import eventlet
-        async_mode = 'eventlet'
-    except ImportError:
-        pass
-
-    if async_mode is None:
-        try:
-            from gevent import monkey
-            async_mode = 'gevent'
-        except ImportError:
-            pass
-
-    if async_mode is None:
-        async_mode = 'threading'
-
-    print('async_mode is ' + async_mode)
-
-# monkey patching is necessary because this application uses a background
-# thread
-if async_mode == 'eventlet':
-    import eventlet
-    eventlet.monkey_patch()
-elif async_mode == 'gevent':
-    from gevent import monkey
-    monkey.patch_all()
+import os
+from gevent import monkey
+monkey.patch_all()
 
 import time, string, random
 from threading import Thread
-from flask import Flask, render_template, session, request, redirect
-from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, disconnect
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app)
 thread = None
 
 
 def background_thread():
     """Example of how to send server generated events to clients."""
-    x = 0
+    count = 0
+    while True:
+        time.sleep(10)
+        count += 1
+        socketio.emit('my response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
 
 @app.route('/')
 def index():
     global thread
     if thread is None:
         thread = Thread(target=background_thread)
-        thread.daemon = True
         thread.start()
     return render_template('index.html')
 
@@ -109,4 +84,4 @@ def connect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 33507)))
